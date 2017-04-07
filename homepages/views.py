adf2d827dc19e10c5ad6c4632f8da7b8544c02a1
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-04-07 17:56:49 +0300

    page graphics html

--- homepages/views.py
+++ homepages/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'homepages/home.html')
+
+def graphics(request):
+    return render(request,'grafic.html')
