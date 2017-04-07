841829ad2f4e6b6403fe397e2e4a9fb52e4193bf
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-04-07 20:06:28 +0300

    sass config for django templates

--- homepages/views.py
+++ homepages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'homepages/home.html')
+    return render(request,'homepages/index.html')
 
 def graphics(request):
     return render(request,'grafic.html')
--- pythonic/settings.py
+++ pythonic/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+SASS_PROCESSOR_ENABLED = True
 SASS_PROCESSOR_INCLUDE_DIRS = [
     os.path.join(BASE_DIR, 'static/scss'),
 ]
